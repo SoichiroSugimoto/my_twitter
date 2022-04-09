b3c343e070961ffddd01225999292f292147e6c8
SoichiroSugimoto <fcsoichi1998@gmail.com>
2022-04-09 18:31:35 +0900

    add show each user's post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,17 +1,28 @@
 class PostsController < ApplicationController
+	before_action :correct_user, only: :destroy
+
+	def index
+		@posts = Post.all
+	end
+
 	def create
 		@post = current_user.posts.build(post_params)
 		@post.save
-		redirect_to "/users/#{params[:id]}"
+		redirect_to "/users/#{@post.user_id}"
 	end
 
 	def destroy
-		post = Post.find(params[:id])
-		post.destroy
+		@post.destroy
+		redirect_to "/users/#{@post.user_id}"
 	end
 
 	private
 		def post_params
 			params.require(:post).permit(:body)
+		end
+
+		def correct_user
+			@post = current_user.posts.find_by(id: params[:id])
+			redirect_to root_url if @post.nil?
 		end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -28,7 +28,7 @@ class UsersController < ApplicationController
 	def show
 		@user = User.find(params[:id])
 		@post = current_user.posts.build
-		@posts = current_user.posts.all
+		@microposts = Post.all
 	end
 
 	private
